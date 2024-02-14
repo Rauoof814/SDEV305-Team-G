8fe31a39abf57ab36a05455d9c930172b57f05cf
cheesy d <cheesyd64@hotmail.com>
2024-02-13 18:50:50 -0800

    Adjusted $updates variable and links to pages

--- sprint3/newApplicationAddtoDb.php
+++ sprint3/newApplicationAddtoDb.php
@@ -21,14 +21,14 @@
     <div class="container">
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand fs-3" href="dashboard.html">GRC ATT</a>
+                <a class="navbar-brand fs-3" href="dashboard.php">GRC ATT</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText">
                     <span class="navbar-dark navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse fs-3" id="navbarText">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link" href="dashboard.html">Dashboard</a>
+                            <a class="nav-link" href="dashboard.php">Dashboard</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="newApplicationForm.html">New Application</a>
@@ -41,7 +41,7 @@
                                 Admin
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item fs-5" href="adminDashboard.html">Admin Dashboard</a></li>
+                                <li><a class="dropdown-item fs-5" href="adminDashboard.php">Admin Dashboard</a></li>
                                 <li><a class="dropdown-item active fs-5" href="adminAnnouncement.html">Admin Announcement</a></li>
                             </ul>
                         </li>
@@ -84,7 +84,8 @@
             echo '<p>Updates: ' . $updates . '</p>';
         }
         else{
-            echo '<p>Updates: No updates at this time.</p>';
+            $updates = "No updates at this time.";
+            echo "<p>Updates: $updates</p>";
         }
         echo '<p>Status: ' . $status . '</p>';
         echo '<p>Follow up on: ' . $followUpDate . '</p>';
