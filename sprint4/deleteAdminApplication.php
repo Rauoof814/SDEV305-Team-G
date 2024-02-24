d1226b5e1e61157b62acf318340ec7e610071093
PVR <ptreyes2@gmail.com>
2024-02-24 12:57:19 -0800

    updated deletion.php for users and admins, need to tweak dashboard & adminDashboard for these to work

--- sprint4/deleteAdminApplication.php
+++ sprint4/deleteAdminApplication.php
@@ -1,17 +1,12 @@
 <?php
-// Establishing database connection
-$username = 'gnocchig';
-$password = '0I(gjj4L!6a1PK';
-$hostname = 'localhost';
-$database = 'gnocchig_test';
-
-$conn = @mysqli_connect($hostname, $username, $password, $database) or die("Error Connecting to DB: " . mysqli_connect_error());
+include 'attdb.php';
 
 // Delete application for admin
 if(isset($_POST['delete_application_admin'])) {
     $application_id = $_POST['application_id'];
 
-    // update statement to set is_deleted to 1
+    // Update statement to set is_deleted to 1
+    // Need to create table column named is_deleted for this to work properly
     $sql = "UPDATE applications SET is_deleted = 1 WHERE id = ?";
     $stmt = mysqli_prepare($conn, $sql);
 
